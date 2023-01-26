36d23fd1948d1974fc60b1e6be3341b5909dde6b
unknown <Dennis Okafor>
2023-01-26 13:36:48 +0100

    created the aboutus, services and contact us templates

--- App.py
+++ App.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__)    # set FLASK_APP=App.py    flask run
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index1.html')
+
+@app.route('/index1.html')
+def index():
+    return render_template('index1.html')
+
+@app.route('/about.html')
+def About():
+    return render_template('about.html')
+
+
+@app.route('/services.html')
+def Services():
+    return render_template('services.html')
+
+
+@app.route('/contact.html')
+def Contact():
+    return render_template('contact.html')
 
 
 
